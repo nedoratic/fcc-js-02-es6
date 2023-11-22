a7ce9cfe81ab494126bc59146335543342541d56
nedoratic <nedoratic@gmail.com>
2023-11-22 12:48:48 +0100

    import everything from a file

--- 022-reuse-javascript-code-using-import/script.js
+++ 022-reuse-javascript-code-using-import/script.js
@@ -8,6 +8,8 @@
 
 // import { add, subtract } from './math_functions.js';
 
+// TASK
+
 // Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
 
 // import { lowercaseString, uppercaseString} from './string_functions.js';
